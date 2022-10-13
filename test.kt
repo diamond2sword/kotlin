4c44c750db6ecbb91bfac3ddafb76f9ef6df25d8
diamond2sword <95223065+diamond2sword@users.noreply.github.com>
2022-10-13 11:41:09 +0800

    Update test.kt

--- test.kt
+++ test.kt
@@ -1,1 +1,37 @@
+```Kotlin
+fun main() {
+    val foo = Foo(BAR3, BAZ1)
+    println(foo)
 
+    val faa = Foo(BAR2, BAZ1)
+    println(faa)
+
+    val fii = Foo(BAR3, BAZ1)
+    println(fii)
+}
+
+data class Foo(bar : Bar, baz : Baz) : I<Foo> {
+    
+    enum class Bar : I<Bar> {
+        BAR1,
+        BAR2,
+        BAR3
+    }
+
+    enum class Baz : I<Baz> {
+        BAZ1,
+        BAZ2,
+        BAZ3
+    }
+}
+
+class I<T : I<T>> {
+    fun <F : T> F.isEqualTo(f : F) : Boolean {
+        return this == f
+    }
+
+    fun <F : T> F.isIn(f : List<F>) : Boolean {
+        return f in this
+    }
+}
+```
